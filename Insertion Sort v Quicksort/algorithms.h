fb4b7579b109666c6e5d05ef75e34f3b1e2be0b4
Cook-48427 <60040038+Cook-48427@users.noreply.github.com>
2021-09-07 01:40:52 -0400

    optimized modifed pivot

--- Insertion Sort v Quicksort/algorithms.h
+++ Insertion Sort v Quicksort/algorithms.h
@@ -11,6 +11,18 @@ public:
 		sorter_quick(collection, collection.begin(), --collection.end(), getPivot);
 	};
 
+	static void quickSort_modified(std::list<int>& collection) {
+		sorter_quick(collection, collection.begin(), --collection.end(), getPivot_modified);
+	};
+
+	static void quickSort_alternate(std::list<int>& collection) {
+		sorter_quick_alternate(collection, collection.begin(), --collection.end(), getPivot);
+	};
+
+	static void quickSort_modified_alternate(std::list<int>& collection) {
+		sorter_quick_alternate(collection, collection.begin(), --collection.end(), getPivot_modified);
+	};
+
 	static void sorter_quick(std::list<int>& collection, std::list<int>::iterator begin, std::list<int>::iterator end, std::function<std::list<int>::iterator(std::list<int>::iterator, std::list<int>::iterator)> pivotPoint) {
 		if (collection.size() <= 1) {
 			return;
@@ -51,31 +63,6 @@ public:
 		}
 	}
 
-	static std::list<int>::iterator getPivot(std::list<int>::iterator begin, std::list<int>::iterator end) {
-		return end;
-	}
-
-	static std::list<int>::iterator getPivot_modified(std::list<int>::iterator begin, std::list<int>::iterator end) {
-		const int count = 3;
-		int pivotIndecies[count];
-		const int length = std::distance(begin, end);
-		for (int i = 0; i < count; i++) {
-			pivotIndecies[i] = rand() % length;
-		}
-		std::sort(pivotIndecies, pivotIndecies + count);
-		auto pivot = begin;
-		std::advance(pivot, pivotIndecies[count/2]);
-		auto temp = *end;
-		*end = *pivot;
-		*pivot = temp;
-		pivot = end;
-		return pivot;
-	}
-
-	static void quickSort_modified(std::list<int>& collection) {
-		sorter_quick(collection, collection.begin(), --collection.end(), getPivot_modified);
-	};
-
 	static void insertSort(std::list<int>& collection) {
 		collection.push_front(-11);
 		for (auto i = std::next(collection.begin(), 2); i != collection.end(); i++) {
@@ -90,14 +77,6 @@ public:
 			}
 		}
 		collection.erase(collection.begin());
-	};
-
-	static void quickSort_alternate(std::list<int>& collection) {
-		sorter_quick_alternate(collection, collection.begin(), --collection.end(), getPivot);
-	};
-
-	static void quickSort_modified_alternate(std::list<int>& collection) {
-		sorter_quick_alternate(collection, collection.begin(), --collection.end(), getPivot_modified);
 	};
 
 	static void sorter_quick_alternate(std::list<int>& collection, std::list<int>::iterator begin, std::list<int>::iterator end, std::function<std::list<int>::iterator(std::list<int>::iterator, std::list<int>::iterator)> pivotPoint) {
@@ -123,12 +102,49 @@ public:
 		//Recurse if front is larger than two
 		if (std::distance(begin, lower) > 1) {
 			sorter_quick_alternate(collection, begin, std::prev(lower), pivotPoint);
-			
 		}
 		//Recurse if larger than 1
-		
 		if (lower != pivot) {
 			sorter_quick_alternate(collection, std::next(lower), pivot, pivotPoint);
 		}
 	};
+
+	static std::list<int>::iterator getPivot(std::list<int>::iterator begin, std::list<int>::iterator end) { return end; }
+
+	static std::list<int>::iterator getPivot_modified(std::list<int>::iterator begin, std::list<int>::iterator end) {
+		const int count = 3;
+		std::list<int>::iterator pivotIndecies[count];
+		std::list<int>::iterator pivot;
+		const int length = std::distance(begin, end);
+		for (int i = 0; i < count; i++) {
+			int test = rand();
+			pivotIndecies[i] = std::next(begin, test % length);
+		}
+		if (*pivotIndecies[1] < *pivotIndecies[0]) {
+			if (*pivotIndecies[2] > *pivotIndecies[0]) {
+				pivot = pivotIndecies[0];
+			}
+			else if (*pivotIndecies[1] < *pivotIndecies[2]) {
+				pivot = pivotIndecies[1];
+			}
+			else {
+				pivot = pivotIndecies[2];
+			}
+		}
+		else {
+			if (*pivotIndecies[2] > *pivotIndecies[1]) {
+				pivot = pivotIndecies[1];
+			}
+			else if (*pivotIndecies[0] < *pivotIndecies[2]) {
+				pivot = pivotIndecies[0];
+			}
+			else {
+				pivot = pivotIndecies[2];
+			}
+		}
+		auto temp = *end;
+		*end = *pivot;
+		*pivot = temp;
+		return end;
+	}
 };
