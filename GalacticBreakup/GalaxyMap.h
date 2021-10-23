5262429e2c6a0eee788c9c0565dcef7c0577fcfc
Joshua Grieve <joshua.grieve19@gmail.com>
2021-10-22 23:57:32 -0700

    Galaxy map implemented

--- GalacticBreakup/GalaxyMap.h
+++ GalacticBreakup/GalaxyMap.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <vector>
-#include <map>
+#include <list>
 
 struct Domain
 {
 	char kingdom;
 	Domain* parent;
-	unsigned rank;
+	int rank;
 };
 
 class GalaxyMap
@@ -14,13 +14,18 @@ class GalaxyMap
 public:
 
 	void create(unsigned x, unsigned y, unsigned z);
+	void reset();
+
 	void addKingdom(char id, const std::vector<int>& domains);
 	void createSets();
+
+	unsigned countKingdomSplits(char id) const;
+	void mergeKingdoms(unsigned id1, unsigned id2);
 
 private:
 	unsigned _length;
 	std::vector<std::vector<std::vector<Domain>>> _map;
-	std::map<char, Domain*> _roots;
+	std::list<Domain*> _roots;
 
 
 };
