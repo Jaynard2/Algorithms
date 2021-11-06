d221cb57fb6bbe8c0fc9181a1a4652c1a0953319
Cook-48427 <60040038+Cook-48427@users.noreply.github.com>
2021-11-06 14:39:53 -0400

    Testing class v1

--- RushHour/RushHourTester.h
+++ RushHour/RushHourTester.h
@@ -3,23 +3,26 @@
 #include <map>
 #include <vector>
 
+enum Vehichle {
+	Car = 2,
+	Truck = 3
+};
+
 class RushHourTester {
 public:
 
-	void addVehicle(int x, int y, Vehichle type);
+	void addVehicle(int x, int y, char orient, Vehichle type);
 	bool Test();
 
 	std::string popError();
 
 private:
-	std::map<std::string, std::pair<std::string, std::string>> _SearchResults;
+	std::map<std::string, std::pair<int, std::string>> _SearchResults;
 	std::string _Error;
 
-	std::string cordianteHash(int x, int y, Vehichle type);
 
-};
-
-enum Vehichle {
-	Car = 2,
-	Truck = 3
+	char cordianteHash(int x, int y, char orient, Vehichle type);
+	void unHash(char pos);
+	std::string SearchBoard();
+	void buildBoard(std::string);
 };
