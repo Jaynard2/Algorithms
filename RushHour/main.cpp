384ac2b2c07c3d9dfba3671160c9bef849504dce
Cook-48427 <60040038+Cook-48427@users.noreply.github.com>
2021-11-07 00:33:11 -0400

    io tester

--- RushHour/main.cpp
+++ RushHour/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "RushHourTester.h"
 
 int main()
@@ -36,6 +37,16 @@ int main()
 	{
 		std::cout << game.popError() << std::endl;
 	}
+
+	auto re = game.Results();
+
+	auto iter = re.rbegin();
+	int i = 0;
+	while (iter != re.rend()) {
+		std::cout << *iter << " " << i++  << std::endl;
+		iter++;
+	}
+
 
 	return 0;
 }
